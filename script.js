fc98b357bbfda0a7fdb61dc980a50b5730223729
Ezekiel <zekejenyart@gmail.com>
2020-06-19 19:57:52 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,23 +1,41 @@
-$("#currentDay").text(moment().format("MMMM Do YYYY"));
+// Set the time to appear first in Day, Month, Date
+
+$("#currentDay").text(moment().format("dddd MMMM Do"));
+
+// Create variables for dynamically spawn the table
+
+var timeBlock = $("<div>")
+var hourEl = $("<div>")
+var description = $("<textarea>")
+var saveBtn = $("<button>")
+var saveIcon = $("<i>")
+// set variable to receive the data from local storage in an object
+var retrievedData = JSON.parse(localStorage.getItem("saved Data")) || {};
 
 
-// create a row for column labels
-// create a for loop to create 8 time block rows
-// use $() to create 
-
+// create a function to build the table dynamically every time the page refreshes
 function buildRow(hour) {
     var NOW = moment(hour, "H");
-
+    var action = retrievedData[hour]
     var timeBlock = $("<div>")
-    timeBlock.addClass("row time-block")
+    timeBlock.addClass("row  time-block")
+    timeBlock.attr("data-hour", hour)
 
     var hourEl = $("<div>")
     hourEl.addClass("hour col-1")
     hourEl.text(NOW.format("hA"))
 
     var description = $("<textarea>")
-    description.addClass("description present col-10")
-
+    description.addClass("description col-10")
+        if (moment().valueOf == NOW.value)
+        description.addClass("present")
+         else if (moment().valueOf > NOW.value)
+        description.addClass("past");
+        else {
+        description.addClass("future")
+        }
+    description.val(action)
+   
     var saveBtn = $("<button>")
     saveBtn.addClass("saveBtn col-1")
 
@@ -27,8 +45,38 @@ function buildRow(hour) {
     saveBtn.append(saveIcon)
     timeBlock.append(hourEl, description, saveBtn)
     $(".container").append(timeBlock)
+
+
+    
+
 }
 
 for (var i = 9; i <= 17; i++) {
     buildRow(i)
 }
+// var current = moment()
+// if (current === moment()){
+//     description.addClass("present");
+
+// }else if (current.isAfter(moment()))
+//     descripting.addClass("past")
+
+
+$("button").on("click", function(){
+
+    var bigButt = $(this).parent()
+    var buttAtt = bigButt.attr("data-hour")
+    var buttSoFind = bigButt.find("textarea").val()
+
+    retrievedData[buttAtt] = buttSoFind
+    
+    localStorage.setItem("saved Data",JSON.stringify(retrievedData))
+    console.log(typeof(retrievedData))
+
+
+
+
+})
+console.log(typeof(retrievedData))
+
+
